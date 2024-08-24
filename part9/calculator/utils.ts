9fb03788a971860fd6a94a1705c89fe81cdeed4e
Jiawei Liu <jiawei@Jiaweis-MacBook-Air.local>
2024-08-24 17:24:52 +0100

    Fixed symlink issue and updated directories

--- part9/calculator/utils.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isNotNumber = (argument: any): boolean =>
-    isNaN(Number(argument));
-  
-export default "this is the default...";
