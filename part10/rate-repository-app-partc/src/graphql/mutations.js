eeada70b3dbde1c4414d30360a8ede4421c425e1
jiawei64850 <162934822+jiawei64850@users.noreply.github.com>
2024-08-27 14:40:41 +0100

    Delete part10/rate-repository-app-partc directory

--- part10/rate-repository-app-partc/src/graphql/mutations.js
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const AUTHENTICATE = gql`
-  mutation Authenticate($credentials: AuthenticateInput){
-  authenticate(credentials: $credentials) {
-    accessToken
-  }
-}
-`;
